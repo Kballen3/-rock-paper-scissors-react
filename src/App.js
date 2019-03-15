767b31d3d171361321e728490a0b057db1bb8e7c
Kaleb Allen <7kaleb29allen@gmail.com>
2019-03-15 07:41:02 -0600

    it works

--- src/App.js
+++ src/App.js
@@ -1,28 +1,104 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+
 import './App.css';
+import { Container, Button } from 'semantic-ui-react';
 
 class App extends Component {
-  render() {
+
+  state = { option: ' ', computer: ''}
+
+  handleRock = () => { 
+    this.setState( state => {
+      let choices = ["Rock", "Paper", "Scissors"]
+      let choice =  "Rock";
+      let rand = choices[Math.floor(Math.random() * choices.length)];
+      if (choice === rand){
+        return { option: "Tie", playerChoice: "Rock", comp: rand }
+      }
+      if (rand === "Scissors"){
+        return { option: "You Win", playerChoice: "Rock", comp: rand }
+      }
+      if (rand === "Paper"){
+        return {option: "You Lose", playerChoice: "Rock", comp: rand }
+      }
+    })
+  }
+
+  handlePaper = () => {
+    this.setState( state => {
+      let choices = ["Rock", "Paper", "Scissors"]
+      let choice = "Paper";
+      let rand = choices[Math.floor(Math.random() * choices.length)];
+      if (choice === rand){
+        // return { option: "TIE" }
+        return { option: "Tie", playerChoice: "Paper", comp: rand }
+ 
+      }
+      if (rand === "Rock") {
+        // return { option: "You Win" }
+        return { option: "You Win", playerChoice: "Paper", comp: rand }
+ 
+      }
+      if (rand === "Scissors"){
+        return { option: "You Lose", playerChoice: "Paper", comp: rand }
+      }
+    })
+  }
+  
+
+  handleScissors = () => {
+    this.setState( state => {
+      let choices = ["Rock", "Paper", "Scissors"]
+      let choice = "Scissors";
+      let rand = choices[Math.floor(Math.random() * choices.length)];
+      if (choice === rand){
+        return { option: "Tie", playerChoice: "Scissors", comp: rand }
+      }
+      if (rand === "Paper"){
+        return { option: "You Win", playerChoice: "Scissors", comp: rand }
+      }
+      if (rand === "Rock"){
+        return { option: "You Lose", playerChoice: "Scissors", comp: rand }
+      }
+    })
+  }
+
+  render = () => {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Container>
+        <h1 style={{marginTop: "25px", display: "flex", justifyContent: "center", }}>This is Rock Paper Scissors</h1>
+        <hr />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+
+    
+        <Button onClick={(e) => this.handleRock(e)}>Rock</Button>
+        <Button onClick={(e) => this.handlePaper(e)}>Paper</Button>
+        <Button onClick={(e) => this.handleScissors(e)}>Scissors</Button>
+
+
+
+
+        <p>{this.state.option}</p>
+        <p>Player Choice: {this.state.playerChoice}</p>
+        <p>Computer Choice: {this.state.comp}</p>
+        <p>Wins:</p>
+        <p>Losses:</p>
+        <p>Ties:</p>
+      </Container>
+
+      
+
+
+      
     );
   }
 }
+  
 
 export default App;
