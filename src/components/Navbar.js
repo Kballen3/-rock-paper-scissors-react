64bdbbff5d4c66b59098e95e00eaea325d61215f
Kaleb Allen <7kaleb29allen@gmail.com>
2019-03-18 07:44:55 -0600

    navbar works

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,8 @@
 import React from "react";
 import { Menu, } from "semantic-ui-react";
+import { Link, } from 'react-router-dom'
 
-const NavBar = () => (
+const Navbar = () => (
   <Menu>
     <Link to="/">
       <Menu.Item>
